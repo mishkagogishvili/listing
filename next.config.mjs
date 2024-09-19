786e0aa32dd31161957121cdaa8242c5386ce39d
mishkagogishvili <mishkagogishvili@yahoo.com>
2024-09-19 18:59:05 +0400

    eerr

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,6 @@ const nextConfig = {
     eslint: {
         ignoreDuringBuilds: true,  // This will disable ESLint during builds
     },
-    typescript: {
-        ignoreBuildErrors: true,
-    },
     trailingSlash: true,  // Optional, can help with routing on Netlify
     basePath: '',  // Optional, adjust if needed for subfolder hosting
 };
