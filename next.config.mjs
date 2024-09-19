62d9ebc5b547e0b48a4f576651b7ebf29be4c3bc
mishkagogishvili <mishkagogishvili@yahoo.com>
2024-09-19 17:16:45 +0400

    modify netlify settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    trailingSlash: true,  // Optional, can help with routing on Netlify
+    basePath: '',  // Optional, adjust if needed for subfolder hosting
+};
 
 export default nextConfig;
