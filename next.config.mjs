0318d1488fc593062728f3278d54532c2fb725e1
mishkagogishvili <mishkagogishvili@yahoo.com>
2024-09-19 17:40:58 +0400

    disable eslint errors on build

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    eslint: {
+        ignoreDuringBuilds: true,  // This will disable ESLint during builds
+    },
     trailingSlash: true,  // Optional, can help with routing on Netlify
     basePath: '',  // Optional, adjust if needed for subfolder hosting
 };
